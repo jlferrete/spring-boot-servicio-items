d47bb710455cddde08905008afe01126fe717635
José Luis Ferrete <josel.ferrete@s-dos.es>
2020-06-29 11:59:30 +0200

    Added REST client FEIGN for API productos

--- src/main/java/com/formacionbdi/springboot/app/item/SpringbootServicioItemApplication.java
+++ src/main/java/com/formacionbdi/springboot/app/item/SpringbootServicioItemApplication.java
@@ -2,7 +2,9 @@ package com.formacionbdi.springboot.app.item;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.openfeign.EnableFeignClients;
 
+@EnableFeignClients
 @SpringBootApplication
 public class SpringbootServicioItemApplication {
 
--- src/main/java/com/formacionbdi/springboot/app/item/controllers/ItemController.java
+++ src/main/java/com/formacionbdi/springboot/app/item/controllers/ItemController.java
@@ -3,6 +3,7 @@ package com.formacionbdi.springboot.app.item.controllers;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
@@ -14,13 +15,14 @@ import com.formacionbdi.springboot.app.item.models.service.ItemService;
 public class ItemController {
 
 	@Autowired
+	@Qualifier("serviceFeign")
 	private ItemService itemService;
 
 	@GetMapping("/listar")
 	public List<Item> listar() {
 		return itemService.findAll();
 	}
-	
+
 	@GetMapping("/ver/{id}/cantidad/{cantidad}")
 	public Item detalle(@PathVariable Long id, @PathVariable Integer cantidad) {
 		return itemService.findById(id, cantidad);
