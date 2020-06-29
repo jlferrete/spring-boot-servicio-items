6055ff1f40b28a8cc60f89bae5f2f7c4218e5de6
José Luis Ferrete <josel.ferrete@s-dos.es>
2020-06-29 13:05:38 +0200

    Testing load balancing in postman

--- src/main/java/com/formacionbdi/springboot/app/item/models/Producto.java
+++ src/main/java/com/formacionbdi/springboot/app/item/models/Producto.java
@@ -8,6 +8,7 @@ public class Producto {
 	private String nombre;
 	private Double precio;
 	private Date createAt;
+	private Integer port;
 
 	public Long getId() {
 		return id;
@@ -40,5 +41,15 @@ public class Producto {
 	public void setCreateAt(Date createAt) {
 		this.createAt = createAt;
 	}
+
+	public Integer getPort() {
+		return port;
+	}
+
+	public void setPort(Integer port) {
+		this.port = port;
+	}
+	
+	
 
 }
