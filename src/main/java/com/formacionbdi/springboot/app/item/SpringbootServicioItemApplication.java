f7557340aa783809d7739137037a34238d90d65e
José Luis Ferrete <josel.ferrete@s-dos.es>
2020-06-29 14:27:26 +0200

    Connecting our services as Eureka customers

--- src/main/java/com/formacionbdi/springboot/app/item/SpringbootServicioItemApplication.java
+++ src/main/java/com/formacionbdi/springboot/app/item/SpringbootServicioItemApplication.java
@@ -2,10 +2,10 @@ package com.formacionbdi.springboot.app.item;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.netflix.ribbon.RibbonClient;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 
-@RibbonClient(name="servicio-productos")
+@EnableEurekaClient
 @EnableFeignClients
 @SpringBootApplication
 public class SpringbootServicioItemApplication {
