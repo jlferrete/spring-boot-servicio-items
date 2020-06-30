a0ee524c14dd0b4ea895d83e7e6230db00866d67
José Luis Ferrete <josel.ferrete@s-dos.es>
2020-06-30 14:13:46 +0200

    Setting up profiles in service items

--- src/main/java/com/formacionbdi/springboot/app/item/controllers/ItemController.java
+++ src/main/java/com/formacionbdi/springboot/app/item/controllers/ItemController.java
@@ -1,9 +1,17 @@
 package com.formacionbdi.springboot.app.item.controllers;
 
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.core.env.Environment;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
@@ -15,10 +23,18 @@ import com.netflix.hystrix.contrib.javanica.annotation.HystrixCommand;
 
 @RestController
 public class ItemController {
+	
+	private static Logger log = LoggerFactory.getLogger(ItemController.class);
+	
+	@Autowired
+	private Environment env;
 
 	@Autowired
 	@Qualifier("serviceFeign")
 	private ItemService itemService;
+	
+	@Value("${configuracion.texto}")
+	private String texto;
 
 	@GetMapping("/listar")
 	public List<Item> listar() {
@@ -43,6 +59,23 @@ public class ItemController {
 		
 		return item;
 		
+	}
+	
+	@GetMapping("/obtener-config")
+	public ResponseEntity<?> obtenerConfig(@Value("${server.port}") String puerto){
+		
+		log.info(texto);
+		
+		Map<String, String> json = new HashMap<>();
+		json.put("texto", texto);
+		json.put("puerto", puerto);
+		
+		if(env.getActiveProfiles().length>0 && env.getActiveProfiles()[0].equals("dev")) {
+			json.put("autor.nombre", env.getProperty("configuracion.autor.nombre"));
+			json.put("autor.email", env.getProperty("configuracion.autor.email"));
+		}
+		
+		return new ResponseEntity<Map<String, String>>(json, HttpStatus.OK);
 	}
 
 }
